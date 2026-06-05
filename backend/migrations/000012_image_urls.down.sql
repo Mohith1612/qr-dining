e65810a7dadb6d570e0e027457d8e743a8f2a273
Mohith1612 <mohith.dev16@gmail.com>
2026-06-05 18:20:21 +0530

    add 000012_image_urls migration

---
+++ backend/migrations/000012_image_urls.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE menu_items DROP COLUMN IF EXISTS image_url;
+ALTER TABLE restaurants DROP COLUMN IF EXISTS logo_url;
---
+++ backend/migrations/000012_image_urls.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE menu_items
+  ADD COLUMN image_url TEXT;
+
+ALTER TABLE restaurants
+  ADD COLUMN logo_url TEXT;
